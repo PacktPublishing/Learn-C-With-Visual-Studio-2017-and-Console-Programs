abe9b150a4cb7ed9b3c7bbb7b6c82f4b23bb7731
packtbipin2377 <44016603+packtbipin2377@users.noreply.github.com>
2019-05-08 12:54:48 +0530

    Code Added
    Code Added1

--- Lesson2FirstProgram/Lesson2FirstProgram/Lesson2FirstProgram/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿class Program
-{
-    static void  Main()
-    {
-
-
-
-    }
-}
--- Lesson41DebuggerWithForLoops/Lesson41DebuggerWithForLoops/Lesson41DebuggerWithForLoops/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using static System.Console;
-class Program
-{
-    static void Main()
-    {
-        for (int i=1;i<=3;i++)
-            WriteLine(i);
-    }
-}
